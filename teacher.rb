0b4bd81c1a0b5f8a56b26f34bdc66538f04864d5
Esmatullah <esmatqadeemi12@gmail.com>
2023-11-17 07:58:36 +0100

    Fix the linter error

--- teacher.rb
+++ teacher.rb
@@ -2,8 +2,8 @@ require_relative 'person'
 
 class Teacher < Person
   attr_accessor :specialization
-
-  def initialize(id, age, specialization, name='Unknown')
+  
+  def initialize(id, age, name = 'Unknown')
     super(id, age, name)
     @specialization = specialization
   end
